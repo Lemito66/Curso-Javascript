86dbe62bf8714932b37b0df4ca51d70b7eab96f4
Lemito66 <lemito66@gmail.com>
2021-03-17 23:31:19 -0500

    Empezando

---
+++ Sección 3/app.js
@@ -0,0 +1,11 @@
+
+
+console.log('Hola Juan');
+
+ var farenheit=90;
+ let celcious=43;
+//Formula C=(F-32)*(5/9)
+var respuesta= (farenheit-32)*(5/9)
+var farenheitRespuesta=(celcious*(9/5))+32
+console.log("90 grados Farenheit a centigrados es: "+respuesta )
+console.log("43 grados Centigrados a Farenheit es: "+farenheitRespuesta) 
